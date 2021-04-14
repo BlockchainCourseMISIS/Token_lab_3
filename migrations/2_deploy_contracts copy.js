272b4023532d72be3de2489f73bed9691e5edc9c
Данила Комлев <m1902452@edu.misis.ru>
2021-04-14 22:08:09 +0300

    token

--- migrations/2_deploy_contracts copy.js
+++ migrations/2_deploy_contracts copy.js
@@ -1,5 +1,8 @@
 const BlackJack = artifacts.require("BlackJack");
+const Token20 = artifacts.require("TokenERC20");
+module.exports = async function (deployer) {
+   await deployer.deploy(Token20, 1, "token", "token2");
+   const token = await Token20.deployed();
+   await deployer.deploy(BlackJack, token.address);
 
-module.exports = function (deployer) {
-  deployer.deploy(BlackJack);
 };

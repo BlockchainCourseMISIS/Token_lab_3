d143ec2f3d0c7339380d35a0ad9eaeb46be9bd77
Данила Комлев <m1902452@edu.misis.ru>
2021-05-14 16:43:28 +0300

    add before each

--- test/fishka_token.js
+++ test/fishka_token.js
@@ -3,60 +3,81 @@ const { assert } = require("chai");
 const FishkaToken = artifacts.require("FishkaToken");
 const BlackJack = artifacts.require("BlackJack");
 
-contract("stand test", async (accounts) => {
+contract("Black Jack", async (accounts) => {
+  describe("test token", function(){
+  let fishka_token;
+  let black_jack;
+  beforeEach("delpoy", async() =>{
+    fishka_token = await FishkaToken.deployed();
+    black_jack = await BlackJack.deployed();
+  });
   it("transfer test", async () => {
-    const instance = await FishkaToken.deployed();
-    await instance.transfer(accounts[1], 5);
-    const balanceOf0 = await instance.balanceOf(accounts[0]);
-    const balanceOf1 = await instance.balanceOf(accounts[1]);
+    await fishka_token.transfer(accounts[1], 5);
+    const balanceOf0 = await fishka_token.balanceOf(accounts[0]);
+    const balanceOf1 = await fishka_token.balanceOf(accounts[1]);
 
     assert.equal(balanceOf0, 5, "balance of dealer should be equal to 5");
     assert.equal(balanceOf1, 5, "balance of player should be equal to 5");
-    await instance.transfer(accounts[0], 5, { from: accounts[1] });
   });
+  it("choose dealer/player", async () => {
+    await fishka_token.approve(black_jack.address, 2, { from: accounts[0] });
+    await fishka_token.approve(black_jack.address, 2, { from: accounts[1] });
 
-  it("stand test", async () => {
-    const instance = await FishkaToken.deployed();
-    let temp = await BlackJack.deployed();
-    await instance.transfer(accounts[1], 5, { from: accounts[0] });
+    await black_jack.choose_dealer(2, { from: accounts[0] });
+    await black_jack.choose_player(2, { from: accounts[1] });
 
-    await instance.approve(temp.address, 2, { from: accounts[0] });
-    await instance.approve(temp.address, 2, { from: accounts[1] });
-
-    await temp.choose_dealer(2, { from: accounts[0] });
-    await temp.choose_player(2, { from: accounts[1] });
-
-    await temp.stand({ from: accounts[0] });
-    await temp.stand({ from: accounts[1] });
-
-    const p = await temp.getStandP();
-    const d = await temp.getStandD();
-    assert.isTrue(p);
-    assert.isTrue(d);
-
-    await temp.checkWinner();
+    //написать assert
   });
+  it("give cards", async () => {
+    await black_jack.giveCards({ from: accounts[0] });
+    let amount = await black_jack.get_cards_amount();
+    assert.equal(
+      amount.toNumber(),
+      49,
+      "amount of cards should be equal to 49"
+    );
+  });
+  it("hit dealer", async () => {
+    await black_jack.hit_dealer({ from: accounts[0] });
+    let amount = await black_jack.get_cards_amount();
+    assert.equal(
+      amount.toNumber(),
+      48,
+      "amount of cards should be equal to 48"
+    );
+  });
+  it("hit player", async () => {
+    await black_jack.hit_player({ from: accounts[1] });
+    let amount = await black_jack.get_cards_amount();
 
+    assert.equal(
+      amount.toNumber(),
+      47,
+      "amount of cards should be equal to 47"
+    );
+  });
   it("add money", async () => {
-    const instance = await FishkaToken.deployed();
-    let temp = await BlackJack.deployed();
+    await fishka_token.approve(black_jack.address, 3, { from: accounts[0] });
+    await fishka_token.approve(black_jack.address, 3, { from: accounts[1] });
 
-    await instance.approve(temp.address, 2, { from: accounts[0] });
-    await instance.approve(temp.address, 2, { from: accounts[1] });
+    await black_jack.add_money_player(3, { from: accounts[1] });
+    await black_jack.add_money_dealer(3, { from: accounts[0] });
 
-    await temp.choose_dealer(2, { from: accounts[0] });
-    await temp.choose_player(2, { from: accounts[1] });
-
-    await instance.approve(temp.address, 3, { from: accounts[0] });
-    await instance.approve(temp.address, 3, { from: accounts[1] });
-
-    await temp.add_money_player(3, { from: accounts[1] });
-    await temp.add_money_dealer(3, { from: accounts[0] });
-
-    const balance_player = await temp.get_balance_player();
-    const balance_dealer = await temp.get_balance_dealer();
+    const balance_player = await black_jack.get_balance_player();
+    const balance_dealer = await black_jack.get_balance_dealer();
 
     assert.equal(5, balance_dealer, "dealer balance should be equal to 5");
     assert.equal(5, balance_player, "player balance should be equal to 5");
+  });
+  it("stand test", async () => {
+    await black_jack.stand({ from: accounts[0] });
+    await black_jack.stand({ from: accounts[1] });
+
+    const p = await black_jack.get_stand_player();
+    const d = await black_jack.get_stand_dealer();
+    assert.isTrue(p);
+    assert.isTrue(d);
+  });
+
   });
 });
